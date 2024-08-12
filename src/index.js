0c99cb0c6faaca9a93e6ecb43a6adce4406f19c1
ElderDragonfly <ElderDragonfly@yandex.ru>
2024-08-12 21:19:37 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
-// You should implement your task here.
+'use strickt';
+/*jshint esversion: 9 */
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  const result = [];
+  if(!matrix) return [];
+  matrix.forEach((element, index) => {
+    if(index % 2 === 0) {
+      result.push(...element);
+    } else result.push(...element.reverse());
+  });
+  return result;
+};
